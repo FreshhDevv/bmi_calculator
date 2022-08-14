9a49e2b6deea4a66a50a1b6ad8f74f0a790f0a34
FreshDev <billyhans90@gmail.com>
2022-08-15 00:09:36 +0100

    part one of result screen complete

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,3 +1,5 @@
+import 'package:bmi/constants.dart';
+import 'package:bmi/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultsPage extends StatelessWidget {
@@ -9,7 +11,44 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CACULATOR'),
       ),
-      body: Text('Thes are your results'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              child: Text(
+                'Your Results',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 5,
+              child: ReusableCard(
+                colour: kActiveCardColor,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Normal',
+                      style: kResultTextStyle,
+                    ),
+                    Text(
+                      '20.8',
+                      style: kBMITextStyle,
+                    ),
+                    Text(
+                      textAlign: TextAlign.center,
+                      'Your BMI result is quite low, you should eat more',
+                      style: kBodyTextStyle,
+                    ),
+                  ],
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
